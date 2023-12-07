e691cb426c4621cd3c63d489543d31629dc0348e
Eran Kampf <eran@ekampf.com>
2023-12-07 00:59:11 -0800

    fix pointerup

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -1,15 +1,15 @@
 jQuery(document).ready(function($){
 
     var offset = 1250; 
-    var duration = 800; 
+    var duration = 200; 
     jQuery(window).scroll(function() { 
         if (jQuery(this).scrollTop() > offset) { 
-          jQuery('.back-to-top').fadeIn(duration); 
+          jQuery('.pointerup').fadeIn(duration); 
         } else { 
-          jQuery('.back-to-top').fadeOut(duration); 
+          jQuery('.pointerup').fadeOut(duration); 
         }
     });
-    jQuery('.back-to-top').click(function(event) { 
+    jQuery('.pointerup').click(function(event) { 
       event.preventDefault(); 
       jQuery('html, body').animate({scrollTop: 0}, duration); 
       return false; 
